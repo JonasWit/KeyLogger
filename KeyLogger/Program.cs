947a85b0bceecc6c3cce8216e4a6186cff119361
Viers <jonaszwitek@gmail.com>
2020-11-08 23:15:26 +0100

    init

--- KeyLogger/Program.cs
+++ KeyLogger/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Net.Mail;
+using Microsoft.Win32;
 
 namespace KeyLogger
 {
     class Program
     {
+
+
+
+
+
         static void Main(string[] args)
         {
+
+
+
+
+
+
         }
+
+        private static void SystemEvents()
+        { 
+        
+
+
+        }
+
+        private void SendMail()
+        { 
+
+        
+        }
+
+        private void Spread()
+        { 
+        
+        
+        
+        }
+
     }
 }
